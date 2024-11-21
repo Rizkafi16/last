36be6c8eee257f67e7aebaad13dbd565eb1e6af7
Rizka.FI <Rizkafiiii@gmail.com>
2024-11-21 16:11:18 +0700

    first commit

---
+++ my-app/src/services/getAllProducts.js
@@ -0,0 +1,35 @@
+export default function getAllProducts() {
+    return [
+        {
+            id: 'PA-pattero',
+            slug: 'Zebra-pattero-sweater',
+            name: "Zebra Pattero Sweater",
+            category: "Women's Sweater",
+            price: 299_000,
+            stock: 25,
+            imageUrl: 'assets/images/baju/Baju 1.jpg',
+            description: 'Let yourself immersed in a symphony of colors, textures, and silhouettes in a timeless basics. Inviting you to build a wardrobe where simplicity meets sophistication.'
+        },
+        {
+            id: 'ED-pattero',
+            slug: 'Claire-pattero-sweater',
+            name: "Claire Pattero Sweater",
+            category: "Women's Sweater",
+            price: 325_000,
+            stock: 30,
+            imageUrl: 'assets/images/baju/Baju 2.jpg',
+            description: 'Let yourself immersed in a symphony of colors, textures, and silhouettes in a timeless basics. Inviting you to build a wardrobe where simplicity meets sophistication.'
+        },
+        {
+            id: 'ZE-pattero',
+            slug: 'zebra-pattero-sweater',
+            name: "Zebra Pattero Sweater",
+            category: "Women's Sweater",
+            price: 275_000,
+            stock: 13,
+            imageUrl: 'assets/images/baju/Baju 3.jpg',
+            description: 'Let yourself immersed in a symphony of colors, textures, and silhouettes in a timeless basics. Inviting you to build a wardrobe where simplicity meets sophistication.'
+        }
+    ]
+}
+
